fe66b6e8dbdd824d3a08887e082fbc6a26cf5b5f
onex-sarthak <sarthak@onextel.com>
2025-03-05 18:33:01 +0530

    corrected variable neme

--- src/main/java/org/example/db2_pick_app/dto/DlrCallbackRequest.java
+++ src/main/java/org/example/db2_pick_app/dto/DlrCallbackRequest.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 public class DlrCallbackRequest {
     private String recipientMobileNumber;
     private String dlrStatus;
-    private String timestamp;
+    private String dlrErrorCode;
 }
