97add4ec1e2dafd3471dbbbdbad283dcd5a49061
onex-sarthak <sarthak@onextel.com>
2025-04-03 16:39:34 +0530

    Modified webclient to block inside the MessageService and handeled 4xx and 5xx response (#10)

--- src/main/java/org/onextel/db2_pick_app/service/CPaaSIntegrationService.java
+++ src/main/java/org/onextel/db2_pick_app/service/CPaaSIntegrationService.java
@@ -6,9 +6,11 @@ import org.onextel.db2_pick_app.repository.MessageRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
@@ -35,49 +37,45 @@ public class CPaaSIntegrationService {
         this.webClient = webClientBuilder.baseUrl("https://api.smsc.ai").build();
     }
 
-    public boolean sendMessagesInBatch(List<MessageInfo> messages) throws Throwable {
-        try {
-            String messageIdString = messages.stream()
-                    .map(MessageInfo::getUniqueId)
-                    .collect(Collectors.joining(","));
+    public Mono<Boolean> sendMessagesInBatch(List<MessageInfo> messages) {
+        String messageIdString = messages.stream()
+                .map(MessageInfo::getUniqueId)
+                .collect(Collectors.joining(","));
 
-            log.info("Processing batch of {} messages with ids: {}", messages.size(),messageIdString);
-//            messageRepository.updateMessageStatusBatch(messageIdString, 1);
+        log.info("Processing batch of {} messages with ids: {}", messages.size(), messageIdString);
 
-            List<SmsRequest.SmsDetail> smsRequests = messages.stream()
-                    .map(this::createSmsDetail)
-                    .toList();
+        List<SmsRequest.SmsDetail> smsRequests = messages.stream()
+                .map(this::createSmsDetail)
+                .toList();
 
-            //Create the request body
-            SmsRequest requestBody = new SmsRequest(apiAuthToken, smsRequests);
+        SmsRequest requestBody = new SmsRequest(apiAuthToken, smsRequests);
+        log.info("Sending request: {}", requestBody);
 
-            log.info("Sending request : {}",requestBody.toString());
-
-            // This needs to be verified for retry...
-            String response = webClient.post()
-                    .uri("api/jsmslist")
-                    .bodyValue(requestBody)
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .retryWhen(
-                            Retry.fixedDelay(3, Duration.ofSeconds(5)) // Retry up to 3 times with a 5-second delay
-                                    .filter(throwable -> throwable instanceof WebClientResponseException)
-                    )
-                    .doOnSuccess(res -> log.info("API response received successfully: {}", res))
-                    .doOnError(WebClientResponseException.class, ex -> log.error("Error response: {}", ex.getResponseBodyAsString()))
-                    .block();
-
-            log.info("Response : {}", response);
-
-            return true;
-        } catch (WebClientResponseException e) {
-            log.error("Error response: {}", e.getResponseBodyAsString());
-            return false;
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Message batch processing failed", e);
-        }
+        return webClient.post()
+                .uri("/api/jsmslis")
+                .bodyValue(requestBody)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> {
+                    log.error("Client error (4xx) occurred: {}", response.statusCode());
+                    return response.bodyToMono(String.class)
+                            .doOnNext(body -> log.error("Response body: {}", body))
+                            .then(Mono.error(new RuntimeException("4xx error: " + response.statusCode())));
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, response -> {
+                    log.error("Server error (5xx) occurred: {}", response.statusCode());
+                    return response.bodyToMono(String.class)
+                            .doOnNext(body -> log.error("Response body: {}", body))
+                            .then(Mono.error(new RuntimeException("5xx error: " + response.statusCode())));
+                })
+                .bodyToMono(String.class)
+                .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(5))
+                        .filter(throwable -> throwable instanceof WebClientResponseException))
+                .doOnSuccess(response -> log.info("API response received successfully: {}", response))
+                .doOnError(WebClientResponseException.class, ex -> log.error("Error response: {}", ex.getResponseBodyAsString()))
+                .map(response -> true) // Convert response to boolean
+                .onErrorReturn(false); // Return false if an error occurs
     }
+
 
 
     private SmsRequest.SmsDetail createSmsDetail(MessageInfo message) {
