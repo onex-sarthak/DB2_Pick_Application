b88f79adbdf40252e0c46ca39b5433f6a67402de
onex-sarthak <sarthak@onextel.com>
2025-04-10 18:16:30 +0530

    add gson dependency

--- src/main/java/org/onextel/db2_pick_app/util/UUIDGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.onextel.db2_pick_app.util;
-
-import java.util.UUID;
-
-public class UUIDGenerator {
-    public String generateKey() {
-        return UUID.randomUUID().toString();
-    }
-}
