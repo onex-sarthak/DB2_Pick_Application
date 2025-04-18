1163da73a571a0805a915d05a31b63eb44c6f0f7
onex-sarthak <sarthak@onextel.com>
2025-04-18 11:07:39 +0530

    integrated rocks db for service restart

--- src/main/java/org/onextel/db2_pick_app/service/dlr/DlrCallbackService.java
+++ src/main/java/org/onextel/db2_pick_app/service/dlr/DlrCallbackService.java
@@ -1,12 +1,25 @@
 package org.onextel.db2_pick_app.service.dlr;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.onextel.db2_pick_app.dto.DlrCallbackRequestDto;
 import org.onextel.db2_pick_app.repository.DlrCallbackRepository;
 
+import org.onextel.db2_pick_app.service.rocksdb.RocksDBDlrHandler;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.Executor;
 
@@ -14,30 +27,43 @@ import java.util.concurrent.Executor;
 @Service
 public class DlrCallbackService {
 
+    // TODO: Declare these variables in application.yaml and use them here through @Value annotatoin
     private static final int BATCH_SIZE = 1000;
-    private static final long TIMEOUT_MILLISECONDS = 1000;
+    private static final long TIMEOUT_MILLISECONDS = 100000;
 
     private final DlrCallbackBuffer buffer;
     private final DlrBatchScheduler scheduler;
     private final DlrCallbackRepository repository;
     private final Executor flushexecutor;
-
+    private final RocksDBDlrHandler rocksDBDlrHandler;
+    private final ObjectMapper objectMapper;
 
     public DlrCallbackService(
             DlrCallbackBuffer buffer,
             DlrBatchScheduler scheduler,
             DlrCallbackRepository repository,
-            @Qualifier("dlrCallbackExecutor") Executor flushexecutor // 👈 Qualifier used here
+            @Qualifier("dlrCallbackExecutor") Executor flushexecutor, // Qualifier used here
+            RocksDBDlrHandler rocksDBDlrHandler,
+            ObjectMapper objectMapper
     ) {
         this.buffer = buffer;
         this.scheduler = scheduler;
         this.repository = repository;
         this.flushexecutor = flushexecutor;
+        this.rocksDBDlrHandler = rocksDBDlrHandler;
+        this.objectMapper = objectMapper;
     }
 
-    public void addToBatch(DlrCallbackRequestDto request) {
-        boolean shouldFlush = buffer.add(request, BATCH_SIZE);
+    public void addToBatch(DlrCallbackRequestDto request){
 
+        try{
+            String json = objectMapper.writeValueAsString(request);
+            rocksDBDlrHandler.put(request.getSrNo().toString(), json);
+            log.info("Adding DlrCallbackRequestDto to DB: " + json);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        boolean shouldFlush = buffer.add(request, BATCH_SIZE);
         if (shouldFlush) {
             log.info("Batch full, flushing");
             scheduler.cancel();
@@ -54,6 +80,13 @@ public class DlrCallbackService {
         try {
             repository.updateBatch(toProcess);
             log.info("Flushed {} DLR records", toProcess.size());
+
+            //  Delete from RocksDB
+            toProcess.forEach(req -> {
+                String key = String.valueOf(req.getSrNo());
+                rocksDBDlrHandler.delete(key);
+            });
+
         } catch (Exception e) {
             log.error("Failed to flush DLR batch", e);
         }
@@ -62,4 +95,23 @@ public class DlrCallbackService {
             scheduler.schedule(this::flushBatch, TIMEOUT_MILLISECONDS);
         }
     }
+
+    public void processRocksDBDlrData(){
+        List<String> keys = rocksDBDlrHandler.iterate();
+        for (String key : keys) {
+            try{
+                String serializedMessages = rocksDBDlrHandler.get(key);
+                if (serializedMessages != null) {
+                    DlrCallbackRequestDto dlr = objectMapper.readValue(serializedMessages, DlrCallbackRequestDto.class);
+                    log.info("Submitting recovered batch from RocksDB with key: {} and value: {}", key,dlr);
+                    addToBatch(dlr);
+                }
+
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
 }
